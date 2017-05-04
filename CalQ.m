1ad427451f89b22ec1edd5258cfa798322968ff5
luzh14 <luzh14@luzh14deMacBook-Pro.local>
2017-05-04 20:20:47 +0800

    Initial

---
+++ CalQ.m
@@ -0,0 +1,54 @@
+%??????
+H=20;%??
+epsilon=8.85*10^(-12);
+r0=17.10*10^(-3)/2;%????
+L=0.375;%????
+R=L*sqrt(2);%??????????
+Req=R*(4*r0/R).^(1/4);
+r1=15.75*10^(-3)/2;%????
+
+%%????????????
+Ua=220000/sqrt(3);
+Ub=220000/sqrt(3)*exp(-2*pi*1i/3);
+Uc=220000/sqrt(3)*exp(+2*pi*1i/3);
+
+%%???????????
+x1=-3.5;y1=H+6+6-5;
+x2=-4.6;y2=H+6-5;
+x3=-4.1;y3=H-5;
+x4=3.5;y4=H+6+6-5;
+x5=4.6;y5=H+6-5;
+x6=4.1;y6=H-5;
+%?????
+x7=-2.8;y7=H+9-10/3;
+x8=2.8;y8=H+9-10/3;
+
+%???8??????????????????????
+X=[x1 x2 x3 x4 x5 x6 x7 x8 x1 x2 x3 x4 x5 x6 x7 x8];
+Y=[y1 y2 y3 y4 y5 y6 y7 y8 y1 y2 y3 y4 y5 y6 y7 y8];
+
+U=[Ua Ub Uc Ua Ub Uc 0 0];
+
+%????????
+alpha=zeros(8,8);
+for i=1:6
+    alpha(i,i)=log(2*Y(i)/Req);
+end
+for i=7:8
+    alpha(i,i)=log(2*Y(i)/r1);
+end
+for i=1:8
+    for j=1:8
+        if(i==j)
+            continue;
+        end
+        alpha(i,j)=-log(distance(X(i),Y(i),X(j),Y(j)))+log(distance(X(i),Y(i),X(j),-Y(j)));
+    end
+end
+
+alpha=1/2/pi/epsilon.*alpha;
+Q=U/alpha;
+X=[x1 x2 x3 x4 x5 x6 x7 x8 x1 x2 x3 x4 x5 x6 x7 x8];
+Y=[y1 y2 y3 y4 y5 y6 y7 y8 -y1 -y2 -y3 -y4 -y5 -y6 -y7 -y8];
+Q=[Q -Q]
+
---
+++ distance.m
@@ -0,0 +1,2 @@
+function r=distance(x1,y1,x2,y2)
+r=sqrt((x1-x2)^2+(y1-y2)^2);
